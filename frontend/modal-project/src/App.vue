f8d3b9e7802fb595a8e746a142cd68f3030c9d5f
Fattesingh Rane <ranefattesingh@hotmail.com>
2026-05-12 23:09:16 +0530

    Complete Vue Totorial 4

---
+++ frontend/modal-project/src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <h1>{{ message }}</h1>
+  <br>
+  <input type="text" ref="name">
+  <button @click="handleClick">Click Me</button>
+  <br>
+  <Modal />
+</template>
+
+<script>
+import Modal from './components/Modal.vue'
+
+export default {
+  name: 'App',
+  components: { Modal },
+  data() {
+    return {
+      message: 'My First Vue App'
+    }
+  },
+  methods: {
+    handleClick() {
+      this.message = `Hello, ${this.$refs.name.value}!`
+      this.$refs.name.classList.add('active')
+      this.$refs.name.focus()
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+h1 {
+  border-bottom: 1px solid #ddd;
+  display: inline-block;
+  padding-bottom: 10px;
+}
+
+</style>
