e1e804219b4cde62d122de416f69f7774249c20f
Fattesingh Rane <ranefattesingh@hotmail.com>
2026-06-21 22:17:32 +0530

    learn how to write a dockerfile

---
+++ docker-k8s/simpleweb/index.js
@@ -0,0 +1,9 @@
+import express from 'express'
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hi there')
+})
+
+app.listen(8080, () => console.log('Listening on port 8080'))
